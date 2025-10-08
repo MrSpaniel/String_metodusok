fc8cf6f7411bc2ee83d1f757ea0f20b477a52baf
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-10-08 08:24:12 +0200

    Create 01_kis_es_nagybetu.py

---
+++ 01_kis_es_nagybetu.py
@@ -0,0 +1,13 @@
+"""1️⃣ Kis- és nagybetűssé alakítás – névformázás
+Feladat: Kérj be egy felhasználónevet a regisztrációhoz, majd jelenítsd meg háromféleképpen:
+nagybetűs (pl. címkén vagy azonosítóban),
+
+
+kisbetűs (pl. email összehasonlításhoz),
+
+
+csak az első betű nagy (személyes üdvözlésnél).
+"""
+
+felhasznalonev = input("Kérlek add meg a felhasználóneved:")
+nagybetus_szo =
