3f1f2309968640ae4e9aecfa55b9e2858f6b4de8
Fehér Dániel <feher.daniel.sandor@diak.szbi-pg.hu>
2025-10-08 09:28:59 +0200

    gfn

---
+++ 05.py
@@ -0,0 +1,6 @@
+"""5️⃣ Szó elejének/végének ellenőrzése – email ellenőrzés
+Feladat: Kérj be egy email címet a regisztrációhoz, majd ellenőrizd, hogy Gmail-es-e.
+"""
+email = input("Add meg az email címed:")
+
+ellenorzes = 
